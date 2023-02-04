b9092494f0360798403d1cc3d206ed9525ad10f7
Isaaccna <isaac.andrade1231@gmail.com>
2023-02-04 15:12:44 +0000

    done

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -6,6 +6,8 @@
  * the advantages of separating code in this way will become immediately apparent. You will see this in the
  * coming mini-projects.
  */
+
+
 public class GameService {
     /**
      * This variable represents a secret number. This variable can be referenced anywhere within this GameService class.
@@ -27,6 +29,12 @@ public class GameService {
      * @return true if guessNumber is equal to the secretNumber, false otherwise.
      */
     public boolean checkGuess(double guessNumber) {
-        return false;
+        if (guessNumber == secretNumber) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        
     }
 }
